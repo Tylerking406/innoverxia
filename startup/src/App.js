79a6601f22bc0e9db3bf14ade9f2a6c4620d70d5
Arinao Ndou <tylerking406@gmail.com>
2024-11-17 23:35:38 +0200

    react project with mui

---
+++ startup/src/App.js
@@ -0,0 +1,14 @@
+import Button from '@mui/material/Button';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Hello MUI
+      </Button>
+    </div>
+  );
+}
+
+export default App;
